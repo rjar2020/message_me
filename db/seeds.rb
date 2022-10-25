b10fc47f3b6ab8a39dd27cd347b3b1eb321bcd48
rjar2020 <rolando.angelini@factorial.co>
2022-10-25 23:20:53 +0200

    Inplementing User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Rolando", password: "password")
+User.create(username: "Jose", password: "password")
+User.create(username: "Angel", password: "password")
+User.create(username: "Ini", password: "password")
+User.create(username: "Rod", password: "password")
